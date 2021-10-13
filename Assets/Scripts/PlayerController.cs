8f72f12bb0ed10a30fd7b251d6fd063cd7671fc5
UmutCanT <uctekcambusiness@gmail.com>
2021-10-13 18:52:56 +0300

    Physic-changes
    Wall and player interaction changed.
    Ground texture added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     float moveSpeed;
     Transform aimTransform;
     Vector3 camOffset;
+    float horizontalInput;
+    float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -25,16 +27,19 @@ public class PlayerController : MonoBehaviour
         {
             Vector3 mousePos = GetMouseWorldPos(Input.mousePosition, Camera.main);
             aimTransform.eulerAngles = new Vector3(0, AngleOfAim(mousePos), 0);
-            PlayerMovement();
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
             Shooting();
         }
     }
 
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
     void PlayerMovement()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
         transform.Translate(horizontalInput * Vector3.right * moveSpeed * Time.deltaTime);
         transform.Translate(verticalInput * Vector3.forward * moveSpeed * Time.deltaTime);
     }
@@ -68,5 +73,21 @@ public class PlayerController : MonoBehaviour
     Vector3 DirectionOfAim(Vector3 mousePos)
     {
         return (mousePos - transform.position).normalized;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            moveSpeed = 1;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            moveSpeed = GetComponent<PlayerCharacter>().MoveSpeed;
+        }      
     }
 }
